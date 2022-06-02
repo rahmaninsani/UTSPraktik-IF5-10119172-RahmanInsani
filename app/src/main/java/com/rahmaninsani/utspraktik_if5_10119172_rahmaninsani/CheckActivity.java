9b670be86da34d70d6773ecf904483b006b40b28
Rahman Insani <rahmaninsani54@gmail.com>
2022-06-02 19:40:56 +0700

    Initial Commit

---
+++ app/src/main/java/com/rahmaninsani/utspraktik_if5_10119172_rahmaninsani/CheckActivity.java
@@ -0,0 +1,14 @@
+package com.rahmaninsani.utspraktik_if5_10119172_rahmaninsani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+    }
+}
